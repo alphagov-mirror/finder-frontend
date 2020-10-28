93277629f2f4f79077f81888a59f4294a5b60719
Michael Walker <mike@barrucadu.co.uk>
2020-10-28 10:56:00 +0000

    (wip) Use A/B test machinery to show signed in / out header
    This depends on a new release of slimmer.

--- spec/controllers/brexit_checker_controller_spec.rb
+++ spec/controllers/brexit_checker_controller_spec.rb
@@ -51,4 +51,28 @@ describe BrexitCheckerController, type: :controller do
       end
     end
   end
+
+  context "accounts header AB test setup" do
+    subject { get :results }
+
+    %w[LoggedIn LoggedOut].each do |variant|
+      it "Variant #{variant} disables the search field" do
+        with_variant AccountExperiment: variant do
+          expect(response.headers["X-Slimmer-Remove-Search"]).to eq("true")
+        end
+      end
+    end
+
+    it "Variant LoggedIn requests the signed-in header" do
+      with_variant AccountExperiment: :LoggedIn do
+        expect(response.headers["X-Slimmer-Show-Accounts"]).to eq("signed-in")
+      end
+    end
+
+    it "Variant Loggedout requests the signed-out header" do
+      with_variant AccountExperiment: :LoggedIn do
+        expect(response.headers["X-Slimmer-Show-Accounts"]).to eq("signed-out")
+      end
+    end
+  end
 end
